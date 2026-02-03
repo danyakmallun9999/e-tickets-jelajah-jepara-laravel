fa2d4d2a0fd12645759a948455784721f7881711
danyakmallun9999 <danyakmallun9999@gmail.com>
2026-02-03 15:47:50 +0700

    feat: Implement custom login route `/sapujagat` and add corresponding authentication tests and application configuration.

--- routes/auth.php
+++ routes/auth.php
@@ -4,10 +4,10 @@ use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-    Route::get('login', [AuthenticatedSessionController::class, 'create'])
+    Route::get('sapujagat', [AuthenticatedSessionController::class, 'create'])
         ->name('login');
 
-    Route::post('login', [AuthenticatedSessionController::class, 'store']);
+    Route::post('sapujagat', [AuthenticatedSessionController::class, 'store']);
 });
 
 Route::middleware('auth')->group(function () {
